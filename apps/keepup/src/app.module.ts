ace553b435d87bfb5cb66fc807a760232ef70985
tanchohang <tanchohang@gmail.com>
2023-04-08 16:43:47 +0100

    chore(setup): setup mongodb with mongoose database

--- apps/keepup/src/app.module.ts
+++ apps/keepup/src/app.module.ts
@@ -3,12 +3,24 @@ import { UsersModule } from './users/users.module';
 import { ChatModule } from 'apps/chat/src/chat.module';
 import { ActivitiesModule } from './activities/activities.module';
 import { AuthModule } from 'apps/auth/src/auth.module';
-import { CirclesModule } from './circles/circles.module';
 import { PartiesModule } from './parties/parties.module';
 import { CirclesModule } from './circles/circles.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule, ChatModule, ActivitiesModule, CirclesModule, PartiesModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ChatModule,
+    ActivitiesModule,
+    CirclesModule,
+    PartiesModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      'mongodb://root:example@localhost:27017/keepup?authSource=admin',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
