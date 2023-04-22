2a46719057e7e22edbdb5d093ead5291b1ee8eeb
tanchohang <tanchohang@gmail.com>
2023-04-22 13:02:02 +0100

    feat: add verify logic and send meaning full errors

--- apps/auth/src/guards/refreshjwt.guard.ts
+++ apps/auth/src/guards/refreshjwt.guard.ts
@@ -1,27 +1,45 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Reflector } from '@nestjs/core';
-import { IS_PUBLIC_KEY } from '../decorators/jwt.decorator';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import {
+  TokenExpiredError,
+  JsonWebTokenError,
+  NotBeforeError,
+} from 'jsonwebtoken';
 
 @Injectable()
 export class RefreshAuthGuard extends AuthGuard('jwtrefresh') {
-  constructor(private reflector: Reflector) {
+  constructor(private jwtService: JwtService) {
     super();
   }
-  // async canActivate(context: ExecutionContext) {
-  //   return true;
-  // }
 
   canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    if (isPublic) {
-      return true;
+    const request: Request = context.switchToHttp().getRequest();
+    const refreshToken = request.headers.authorization.split(' ')[1];
+    // return this.authService.validateRefreshToken(refreshToken);
+    try {
+      const payload = this.jwtService.verify(refreshToken);
+      if (payload) return super.canActivate(context);
+    } catch (err) {
+      if (
+        err instanceof JsonWebTokenError ||
+        err instanceof NotBeforeError ||
+        err instanceof TokenExpiredError
+      ) {
+        throw new HttpException(err.message, 498);
+      }
+      throw new UnauthorizedException();
     }
+  }
 
-    return super.canActivate(context);
+  async verifyJwt(refreshToken: string) {
+    const payload = await this.jwtService.verify(refreshToken);
+    return payload;
   }
 }
