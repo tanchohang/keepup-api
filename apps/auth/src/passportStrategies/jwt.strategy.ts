0b41cc6bc6e1b431c6c5355ea7c5d8041fbf6fc6
tanchohang <tanchohang@gmail.com>
2023-04-23 09:13:44 +0100

    refactor: return id instead of userId from jwtStrategy

--- apps/auth/src/passportStrategies/jwt.strategy.ts
+++ apps/auth/src/passportStrategies/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    return { id: payload.sub, username: payload.username };
   }
 }
